110e8c6b2df72565f985ee5c388ac39a27f47979
Mark Nosal <mark.c.nosal@gmail.com>
2023-05-26 16:36:51 -0500

    finished lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,51 @@
 #!/usr/bin/env python3
 
 def admin_login(username, password):
-    # your code here
-    pass
+    if username.lower() == 'admin' and password == '12345':
+        return 'Access granted'
+    
+    return 'Access denied'
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif temperature <65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+    
+    return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if (num % 3) == 0:
+        if (num % 5) == 0:
+            return 'FizzBuzz'
+        
+        return 'Fizz'
+    elif (num % 5) == 0:
+        return 'Buzz'
+    
+    return num
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation in ('+', '-', '*', '/'):
+        try:
+            result = eval(f"{num1} {operation} {num2}")
+            return result
+        except ZeroDivisionError:
+            return "Error: Division by zero"
+    else:
+        print('Invalid operation!')
+        return None
+
+# def calculator(operation, num1, num2):
+#     if operation in ('+', '-', '*', '/'):
+#         return eval(f'{num1} {operation} {num2}')
+#     else:
+#         return 'Invalid operation!'
+    
+
+    # if operation == '+' or operation == '-' or operation == '*' or operation == '/':
+    #     return (num1 + operation + num2)
+    # else:
+    #     return 'Invalid operation!'
